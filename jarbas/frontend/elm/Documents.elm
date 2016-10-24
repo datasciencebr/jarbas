2bd41224508e1eb4cdee0c6f0682e0b87d0bc997
Paulo Pires <taralloqueiroiz@gmail.com>
2016-10-24 23:05:44 +0200

    exposing all functions in Documents.elm

--- jarbas/frontend/elm/Documents.elm
+++ jarbas/frontend/elm/Documents.elm
@@ -1,4 +1,4 @@
-module Documents exposing (Msg, Model, model, loadDocuments, getPage, update, updateLanguage, view, totalPages, onlyDigits)
+module Documents exposing (..)
 
 import Char
 import Documents.Fields as Fields
