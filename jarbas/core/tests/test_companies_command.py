662652b708b5bea2bf004ddf30727277409a09d6
ilozuluchris <ilozuluchidiuso@gmail.com>
2018-01-14 20:32:04 +0100

    Trying to fix positional argument issue

--- jarbas/core/tests/test_companies_command.py
+++ jarbas/core/tests/test_companies_command.py
@@ -37,10 +37,9 @@ class TestCreate(TestCommand):
     @patch('jarbas.core.management.commands.companies.Command.save_activities')
     @patch('jarbas.core.management.commands.companies.Command.print_count')
     @patch.object(Company.objects, 'create')
-    def test_save_companies(self, create, print_count, save_activities, rows, lzma):
+    def test_save_companies(self, create, print_count, save_activities,lzma):
         self.command.count = 0
         lzma.return_value = StringIO()
-        rows.return_value = [sample_company_data]
         save_activities.return_value = ([3], [14, 15])
         self.command.path = 'companies.xz'
         self.command.save_companies()
